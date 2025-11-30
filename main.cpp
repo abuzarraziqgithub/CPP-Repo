f08e86ccffe0793032fb254633d0cbb5d3b93a17
abuzarraziqgithub <abuzarraziqo@gmail.com>
2025-11-30 12:36:14 +0500

    copy  constructor explained

--- main.cpp
+++ main.cpp
@@ -20,13 +20,14 @@ class Hero {
     }
 
     void print(){
-        cout<< level << endl;
+        cout << "Health" << "\t" << this->health << endl;
+        cout<<"Level" << "\t" << this->level << endl;
     }
 
     // Constructor: Default constructor initialized when/during creation of instance, with no input paras, no return type { Hero abuzar ---> abuzar.Hero()-> This is default constructor called automatically}
     
     Hero(){
-        // cout << "Contructor Called!";
+        cout << "Default Contructor Called!" << endl;
     }
     
     // Parametarized Constructor
@@ -40,45 +41,78 @@ class Hero {
         this-> level = level;
         this-> health = health;
     }
+
+
+    // Copy constructor
+    Hero(Hero& /*Pass by reference so it can pass the exact object*/ temp) {
+        cout << "Copy constructor called"<< endl;
+        this->health = temp.health;
+        this->level = temp.level;
+    }
+
 };
 
 int main(){
 
-    // static allocation
-    Hero a(10);
-    // Accessing the address of the instance
-    // cout << "Address of a: " << &a << endl;
-    a.print();
+    Hero shafqat;
+    shafqat.setHealth(40);
+    shafqat.level = 'S';
+    shafqat.print();
 
-    // Dynamic allocation
-    Hero *c = new  Hero(20);
-    c->print();
+    // Copy Constructor
+    Hero essa(shafqat);
+    essa.print();
+    // essa.setHealth = shafqat.setHealth;
+    // essa.level = shafqat.level;
 
 
 
-    cout<< "\n";
 
-    // Acessing the data
-    a.setHealth(70);
-    a.level = 'A';
 
-    cout << "A - Health: " << a.getHealth() << "\t" << "A - Level: " << a.level << endl;
 
-    // dynamic allocation
-    Hero *b = new Hero;
 
-    // Accessing the data
-    b->setHealth(40);
-    b->level = 'B';
+
+
+
+
+
+
+
+    // // static allocation
+    // Hero a(10);
+    // // Accessing the address of the instance
+    // // cout << "Address of a: " << &a << endl;
+    // a.print();
+
+    // // Dynamic allocation
+    // Hero *c = new  Hero(20);
+    // c->print();
+
+
+
+    // cout<< "\n";
+
+    // // Acessing the data
+    // a.setHealth(70);
+    // a.level = 'A';
+
+    // cout << "A - Health: " << a.getHealth() << "\t" << "A - Level: " << a.level << endl;
+
+    // // dynamic allocation
+    // Hero *b = new Hero;
+
+    // // Accessing the data
+    // b->setHealth(40);
+    // b->level = 'B';
     
-    // Derefrencing = fetching the actuall values of the adress
-    cout <<"B - Health: " << (*b).getHealth() << "\t" << "B - Level: " << (*b).level << endl;
-    // we can also print/access the values using arrow key
-    cout <<"B - Health(Arrow way): " << b->getHealth() << "\t" << "B - Health(Arrow way):    " << b->level << endl;
+    // // Derefrencing = fetching the actuall values of the adress
+    // cout <<"B - Health: " << (*b).getHealth() << "\t" << "B - Level: " << (*b).level << endl;
+    // // we can also print/access the values using arrow key
+    // cout <<"B - Health(Arrow way): " << b->getHealth() << "\t" << "B - Health(Arrow way):    " << b->level << endl;
 
-    // int *i = new int;
-    // string *str = new string;
-    // cout << sizeof(i) << endl;
-    // cout << str;
+    // // int *i = new int;
+    // // string *str = new string;
+    // // cout << sizeof(i) << endl;
+    // // cout << str;
 
 }
