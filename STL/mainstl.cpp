32f3362ff4f4360421a43f2e1c3db7c82d94fb5a
abuzarraziqgithub <abuzarraziqo@gmail.com>
2025-12-01 12:45:33 +0500

    deques

--- STL/mainstl.cpp
+++ STL/mainstl.cpp
@@ -3,18 +3,7 @@
 using namespace std;
 
 int main(){
-    // List gives front operation as well, a container and dynamic in nature
-    // Insert in a vector is costly
-    list<int> ls;
-    // In List, the internal operation is a doubly link list(a doubly link list is maintained for a list) and singly list is maintained for vectors.
-    ls.push_back(5); // {5}
-    ls.emplace_back(4); // {5, 4}
-
-    ls.push_front(2); // {2, 5, 4} // in terms of complexity it is very efficient/cheap
-    ls.emplace_front();
-
-    // rest functions are same as vectors
-    // begin, end, rbegin, rend, clear, insert, size, swap etc
+    // Deque is also same as list and vectors, same functions 
     return 0;
 
 }
