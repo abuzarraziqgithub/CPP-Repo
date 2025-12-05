480be767f1c0537e4aa461a9084ef262fbd42675
abuzarraziqgithub <abuzarraziqo@gmail.com>
2025-12-05 16:38:04 +0500

    Refactor running sum implementation to directly modify the input array for improved efficiency.

--- Dev_weekends/Leetcode_sessions/1480/Running_sum_1D_Arr.cpp
+++ Dev_weekends/Leetcode_sessions/1480/Running_sum_1D_Arr.cpp
@@ -74,11 +74,16 @@ class Solution2{
         //     runningSum += nums[i];    
         //     answerArr.push_back(runningSum);
         // }
-        for(int i = 0; i < nums.size(); i++){
-            runningSum += nums[i];
-            nums[i] = runningSum;
+        // for(int i = 0; i < nums.size(); i++){
+        //     runningSum += nums[i];
+        //     nums[i] = runningSum;
+        // }
+        // return nums;
+
+
+        for(int i = 1; i < nums.size(); i++){
+            nums[i] = nums[i] + nums[i - 1];
         }
-        return nums;
 
     }
 
@@ -108,6 +113,5 @@ int main() {
         cout << num << " ";
     }
     cout << endl;
-
 
 }
